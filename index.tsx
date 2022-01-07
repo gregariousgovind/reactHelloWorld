a670337f6a7cfc4568b26c6dd43d628403699ab7
gregariousgovind <govind.chauhan@mountblue.io>
2022-01-07 16:04:55 +0530

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.scss';
@@ -8,14 +8,15 @@ const App = () => {
 
   console.log('App Running....');
 
-  const toggleHandler = () => {
+  const toggleHandler = useCallback(() => {
     setShowPara((prevState) => !prevState);
-  };
+  }, []);
+
   return (
     <div className="container">
       <h1>Hi there!</h1>
       <Hello show={false} />
-      {/* <Hello show={showPara} /> */}
+      <Hello show={showPara} />
       <button onClick={toggleHandler}>Toggle Message!</button>
     </div>
   );
